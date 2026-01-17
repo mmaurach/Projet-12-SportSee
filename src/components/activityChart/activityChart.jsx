9e0b5c842656d2b7f6f1bc27a95115aa7bc87ee2
Maxime Mauracheea <dev.mmaurach@gmail.com>
2026-01-17 15:00:44 +0900

    Modification style ActivityChart suite session de mentorat

--- src/components/activityChart/activityChart.jsx
+++ src/components/activityChart/activityChart.jsx
@@ -43,40 +43,80 @@ const CustomTooltip = ({ active, payload }) => {
   return null;
 };
 
+const RenderCustomizedLegend = () => {
+  return (
+    <div className="LegendWrapper">
+      <div className="LegendPoids">
+        <div
+          className="LegendCircle"
+          style={{ backgroundColor: "black" }}
+        ></div>
+        <div>Poids (kg)</div>
+      </div>
+      <div className="LegendCalories">
+        <div className="LegendCircle" style={{ backgroundColor: "red" }}></div>
+        <div>Calories brûlées (kCal)</div>
+      </div>
+    </div>
+  );
+};
+
 function ActivityChart() {
   return (
     <div className="activity-chart">
       <div className="activity-chart__header">
         <h2>Activité quotidienne</h2>
-        <ul className="activity-chart__legend">
-          <li>
-            <span className="black" /> Poids (kg)
-          </li>
-          <li>
-            <span className="red" /> Calories brûlées (kCal)
-          </li>
-        </ul>
       </div>
 
       <ResponsiveContainer width="100%" height="100%">
-        <BarChart data={activityData} barGap={8}>
-          <CartesianGrid strokeDasharray="2" vertical={false} />
+        <BarChart
+          data={activityData}
+          margin={{
+            top: 40,
+            right: 10,
+            left: 50,
+            bottom: 20,
+          }}
+          barSize={10}
+          barGap={8}
+        >
+          <Legend
+            verticalAlign="top"
+            content={RenderCustomizedLegend}
+            height={50}
+          />
+          <CartesianGrid strokeDasharray="3" vertical={false} horizontal />
           <XAxis
             dataKey="day"
             tickLine={false}
-            tickMargin={15}
+            width={"auto"}
             tick={{ fill: "#9B9EAC", fontSize: 14 }}
+            axisLine={{ stroke: "#d1d2d6" }}
+            padding={{ left: -40, right: -40 }}
+            dy={15}
+            style={{
+              fontWeight: "500",
+              fontSize: "14px",
+              fill: "#9B9EAC",
+            }}
           />
           <YAxis
             yAxisId="kg"
             orientation="right"
             axisLine={false}
             tickLine={false}
-            tickMargin={43}
-            width={65}
+            dx={25}
+            domain={["dataMin -1", "dataMax +2"]}
+            interval={1}
             tick={{ fill: "#9B9EAC", fontSize: 14 }}
           />
-          <YAxis yAxisId="cal" hide />
+          <YAxis
+            yAxisId="cal"
+            orientation="left"
+            axisLine={false}
+            tickLine={false}
+            hide
+          />
           <Tooltip content={<CustomTooltip />} />
           <Bar
             yAxisId="kg"
--- src/data/activityData.js
+++ src/data/activityData.js
@@ -1,9 +1,9 @@
 export const activityData = [
   { day: "1", kilogram: 80, calories: 240 },
-  { day: "2", kilogram: 79, calories: 220 },
+  { day: "2", kilogram: 75, calories: 220 },
   { day: "3", kilogram: 78, calories: 280 },
   { day: "4", kilogram: 78, calories: 290 },
-  { day: "5", kilogram: 77, calories: 160 },
+  { day: "5", kilogram: 75, calories: 160 },
   { day: "6", kilogram: 77, calories: 162 },
   { day: "7", kilogram: 78, calories: 390 },
   { day: "8", kilogram: 79, calories: 360 },
